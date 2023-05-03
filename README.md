2d9921baccf83215d70fc991a1a5d337981df9ad
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-03 16:26:13 +0200

    Add Exercice 2 + update readme

--- README.md
+++ README.md
@@ -1,17 +1,35 @@
 # The-Date-Object
 
-Javascript a un ensemble de fonctionnalités pour gérer et manipuler le temps .
+> Javascript a un ensemble de fonctionnalités pour gérer et manipuler le temps .
 
-
-### Exercise 1
+## Exercise 1
 
 Find the timezones of :
 
-* Anchorage (USA)
-* Reykjavik (Iceland)
-* Saint-Petersburg (Russia)
+> Anchorage (USA)
 
-And display the date and time of these cities along with the time and date of Brussels.
+> Reykjavik (Iceland)
+
+> Saint-Petersburg (Russia)
+
+> And display the date and time of these cities along with the time and date of Brussels.
+
+### Documentation
+
+> <https://www.iana.org/time-zones>
+
+> <https://www.zeitverschiebung.net/en/city/2800866>
+
+> <https://en.wikipedia.org/wiki/List_of_tz_database_time_zones>
+
+## Exercise 2
+
+> Using timestamps, find how many days have passed since the date of your birth. Feeling old, yet?
+
+> Write a function to find how many days have passed since any point in time (after 1970).
+
+### Documentation
+
+> <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getMilliseconds>
 
 
-<https://www.iana.org/time-zones>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 // Exercice 1
 
+const container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container);
+
 function displayTimes() {
-  const container = document.createElement("div");
-  container.classList.add("container");
-  document.body.appendChild(container);
   // Tableau stockant les fuseaux horaires selon la norme IANA
   const timeZones = [
     { label: "Anchorage, Alaska", timeZone: "America/Anchorage" },
@@ -25,3 +26,26 @@ function displayTimes() {
 }
 
 displayTimes();
+
+/*----------------------------------------------------------------*/
+
+// Exercice 2
+
+function displayDays(year, month, day) {
+  // Calcul pour trouver le nombre de millisecondes en un jour
+  const millisecondsPerDay = 24 * 60 * 60 * 1000;
+  const now = new Date();
+  // month-1 car janvier = 0, février = 1 ...
+  const dateCompare = new Date(year, month - 1, day);
+  // getTime() renvoie le nombre de millisecondes depuis le 1er janvier 1970
+  const timeDiff = now.getTime() - dateCompare.getTime();
+  const daysDiff = Math.floor(timeDiff / millisecondsPerDay);
+
+  return daysDiff;
+}
+
+const result = displayDays(1970, 8, 13);
+const daysBorn = document.createElement("p");
+daysBorn.classList.add("color-text");
+daysBorn.innerHTML = `Since my birth it has been: ${result} days but I don't feel old at all :)`;
+container.appendChild(daysBorn);
