543e4d097a2d31b98b5855f2d6fb32d87d5abf77
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-08 17:55:10 +0200

    Add function updateDateTime and update readme

--- Exercice-4/assets/script.js
+++ Exercice-4/assets/script.js
@@ -14,6 +14,11 @@ container.appendChild(dateElement);
 const timeElement = document.createElement("p");
 container.appendChild(timeElement);
 
+const toggleButton = document.createElement('button');
+toggleButton.textContent = '12/24';
+toggleButton.setAttribute("id", "toggleButton");
+container.appendChild(toggleButton);
+
 function updateDateTime() {
     const now = new Date();
 
@@ -27,15 +32,36 @@ function updateDateTime() {
     const dateNow = now.toLocaleString("en-US", dateOptions);
     dateElement.textContent = ` ${dateNow}`;
 
+    const hourFormat = localStorage.getItem('hourFormat');
+
     const timeOptions = {
         hour: 'numeric',
         minute: 'numeric',
         second: 'numeric',
+        hour12: hourFormat === '12',
         timeZone: timeZone.timeZone
     };
+
     const timeNow = now.toLocaleString("en-US", timeOptions);
     timeElement.textContent = ` ${timeNow} ${timeZone.label}`;
 }
 
 updateDateTime();
 setInterval(updateDateTime, 1000);
+
+const toggleButtonElement = document.getElementById('toggleButton');
+toggleButtonElement.addEventListener('click', () => {
+    const hourFormat = localStorage.getItem('hourFormat');
+    let newFormat;
+    if (hourFormat === '12'){
+        newFormat = '24';    
+    }else {
+        newFormat = '12';
+    }   
+    localStorage.setItem('hourFormat', newFormat);
+    updateDateTime();
+});
+
+if (!localStorage.getItem('hourFormat')) {
+    localStorage.setItem('hourFormat', '12');
+};
--- Exercice-4/readme.md
+++ Exercice-4/readme.md
@@ -1,5 +1,11 @@
-## Exercise
+# Exercice 4
 
 Using HTML, CSS (and javascript, of course) reproduce the following picture. This should be centered both horizontaly and vertically in your page.
 
-### Documentation
+ ![Image to be reproduced](https://raw.githubusercontent.com/DelphineLecorney/The-Date-Object/main/Exercice-4/assets/pictures/borne.jpg)
+
+## Documentation
+
+<https://linuxhint.com/display-datetime-in-12-hour-am-pm-format-javascript/>
+
+<https://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format>
