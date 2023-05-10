af9513a3e0604f783b652872735e1873e636fd53
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-10 15:10:45 +0200

    Img 12/24 and test font family

--- Exercice-4/assets/script.js
+++ Exercice-4/assets/script.js
@@ -41,7 +41,7 @@ secondElement.classList.add("secondElement");
 container.appendChild(secondElement);
 
 const toggleButton = document.createElement("button");
-toggleButton.innerHTML = "<img src='../assets/pictures/12_24.jpg' alt='toggle'>";
+toggleButton.innerHTML = "<img src='/Exercice-4/assets/pictures/12_24.jpg' alt='12/24'>";
 toggleButton.setAttribute("id", "toggleButton");
 buttonContainer.appendChild(toggleButton);
 
